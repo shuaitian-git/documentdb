c8c551a58d311663fe2a979a846db9b66826ef60
Parag Jain <paragjain@microsoft.com>
2025-12-18 04:24:52 +0000

    Merged PR 1902598: [oss] Asan Fixes in decimal 128
    ### Does this PR have any customer impact?
    
    ### Type (Feature, Refactoring, Bugfix, DevOps, Testing, Perf, etc)
    
    ### Does it involve schema level changes? (Table, Column, Index, UDF, etc level changes)
    
    ### Are you introducing any new config? If yes, do you have tests with and without them being set?
    
    ### ChangeLog (Refer [Template](../oss/CHANGELOG.md))
    
    ### Description
    [oss] Asan Fixes in decimal 128
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    This pull request is a bug fix addressing ASAN issues in decimal 128 handling.
    
    #### PR Summary
    The pull request removes the `pg_attribute_no_sanitize_alignment()` attribute from several conversion and comparison functions in `/oss/pg_documentdb_core/src/query/bson_compare.c` to ensure proper memory sanitization and alignment correctness.
    - `/oss/pg_documentdb_core/src/query/bson_compare.c`: Removed the attribute from `BsonValueAsInt64WithRoundingMode`.
    - `/oss/pg_documentdb_core/src/query/bson_compare.c`: Removed the attribute from `IsBsonValueNaN`.
    - `/oss/pg_documentdb_core/src/query/bson_compare.c`: Removed the attribute from `IsBsonValueInfinity`.
    - `/oss/pg_documentdb_core/src/query/bson_compare.c`: Removed the attribute from `BsonValueAsDoubleCore`.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    
    Signed-off-by: Shuai Tian <shuaitian@microsoft.com>

--- pg_documentdb_core/src/query/bson_compare.c
+++ pg_documentdb_core/src/query/bson_compare.c
@@ -566,7 +566,7 @@ BsonValueAsInt64(const bson_value_t *value)
  *    - The value is outside of the int64 range.
  * GetBsonDecimal128AsInt64 can throw, see method summary for details.
  */
-pg_attribute_no_sanitize_alignment() int64_t
+int64_t
 BsonValueAsInt64WithRoundingMode(const bson_value_t *value,
 								 ConversionRoundingMode roundingMode,
 								 bool throwIfFailed)
@@ -936,7 +936,7 @@ BsonTypeIsNumber(bson_type_t type)
 
 
 /* returns true if bson value is (double)NaN or (Decimal128)NaN */
-pg_attribute_no_sanitize_alignment() bool
+bool
 IsBsonValueNaN(const bson_value_t *value)
 {
 	if (value->value_type == BSON_TYPE_DECIMAL128)
@@ -957,7 +957,7 @@ IsBsonValueNaN(const bson_value_t *value)
  * Returns 1 if value is Infinity
  * Returns -1 if value is -Infinity
  */
-pg_attribute_no_sanitize_alignment() int
+int
 IsBsonValueInfinity(const bson_value_t *value)
 {
 	if (value->value_type == BSON_TYPE_DECIMAL128)
@@ -1494,7 +1494,7 @@ CompareStrings(const char *left, uint32_t leftLength, const char *right, uint32_
  * Core implementation of converting bson value to double
  * In quiet mode no error is thrown if conversion results in overflow or underflow
  */
-pg_attribute_no_sanitize_alignment() static double
+static double
 BsonValueAsDoubleCore(const bson_value_t *value, bool quiet)
 {
 	switch (value->value_type)
--- pg_documentdb_core/src/types/decimal128.c
+++ pg_documentdb_core/src/types/decimal128.c
@@ -1087,7 +1087,7 @@ RoundOrTruncateDecimal128Number(const bson_value_t *number, int64_t precision,
 /*
  * Performs the specified mathematical operation on x and y and sets it to result.
  */
-pg_attribute_no_sanitize_alignment() static Decimal128Result
+static Decimal128Result
 Decimal128MathematicalOperation2Operands(const bson_value_t *x, const bson_value_t *y,
 										 bson_value_t *result, Decimal128MathOperation
 										 operation)
@@ -1370,7 +1370,7 @@ Decimal128MathematicalOperation1Operand(const bson_value_t *value, bson_value_t 
 /**
  * This method returns the Intel Math Library representation from bson decimal128 representation
  */
-static pg_attribute_no_sanitize_alignment() inline BID_UINT128
+static inline BID_UINT128
 GetBIDUINT128FromBsonValue(const bson_value_t *decimal)
 {
 	CheckDecimal128Type(decimal);
@@ -1399,7 +1399,7 @@ GetBsonDecimal128FromBIDUINT128(const BID_UINT128 *bid)
 /**
  * Checks if value is decimal128 otherwise throws generic error
  */
-pg_attribute_no_sanitize_alignment() static inline void
+static inline void
 CheckDecimal128Type(const bson_value_t *value)
 {
 	if (value->value_type != BSON_TYPE_DECIMAL128)
@@ -1608,7 +1608,7 @@ GetDecimal128FromInt64(int64_t value)
  *
  * Quantize(X, Y) => Returns a quantized decimal128 which has same numerical value as X and exponent as Y
  */
-static pg_attribute_no_sanitize_alignment() bson_decimal128_t
+bson_decimal128_t
 GetBsonValueAsDecimal128Core(const bson_value_t *value, bool shouldQuantizeDouble)
 {
 	if (!BsonValueIsNumberOrBool(value) && value->value_type != BSON_TYPE_DATE_TIME)
